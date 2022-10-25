2a34e40192fd3c392b34175ad1bbef597912ae20
Joseph Madsen <josephmads@protonmail.com>
2022-10-25 09:13:16 -0500

    finished: blog project fully functional

--- blog/views.py
+++ blog/views.py
@@ -6,4 +6,9 @@ from .models import Post
 def index(request):
     posts = Post.objects.all()
     context = {'posts': posts}
-    return render(request, 'blog/index.html', context=context)
+    return render(request, 'blog/index.html', context)
+
+def detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
